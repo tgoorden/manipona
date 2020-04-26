ebc3b5dc77c7e0be63dce7da26e796c9b9727132
Thomas Goorden <tg@eenwereldmetlef.be>
2020-04-26 20:22:22 +0200

    Key exchange

---
+++ src/protocol.js
@@ -0,0 +1,34 @@
+const debug = require('debug')
+
+const hex2buffer = (hex) => Buffer.from(hex, 'hex')
+const buffer2hex = (buffer) => buffer.toString('hex') 
+/**
+ * Attach basic mani pona protocol on top of DHT messaging protocol.
+ **/
+module.exports.attach = (dht, keys, ledger) => {
+  const commands = {
+    hello: (doc, peer) => {
+      dht.sendMessage({ 'message': `Hello ${buffer2hex(peer)}` }, peer)
+    },
+    signKey: (doc, peer) => {
+      dht.sendMessage({ signatureKey: keys.sign.jwk }, peer)
+    },
+    encryptKey: (doc, peer) => {
+      dht.sendMessage({ encryptKey: keys.encrypt.jwk }, peer)
+    }
+  }
+  dht.onmessage((doc, peer) => {
+    if (!doc.command) {
+      debug('Command not present')
+      return
+    }
+    const command = commands[doc.command]
+    if (!command) {
+      debug(`Unkown command: ${command}`)
+    } else {
+      command(doc, peer)
+    }
+  })
+}
+
+
